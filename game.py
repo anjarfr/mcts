28f0bb957ab2b4be640295eba1b5510726a40615
piiingz <ingviun@stud.ntnu.no>
2020-02-25 14:21:57 +0100

    Start nim

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import yaml
 
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
 
 class Game:
     def __init__(self):
@@ -16,16 +19,38 @@ class Game:
 
 
 class Nim(Game):
-    def __init__(self, cfg):
-        self.state = cfg['nim']['n']
-        self.k = cfg['nim']['k']
-    
+    def __init__(self):
+        super().__init__()
+        self.games = cfg['nim']['g']
+        self.start_player = cfg['nim']['p']
+        self.simulations = cfg['nim']['m']
+        self.start_stones = cfg['nim']['n']
+        self.remove_stones = cfg['nim']['k']
+
+        self.remaining_stones = self.start_stones
+
+    def init_game(self):
+        print("Start Pile: {} stones".format(self.start_stones))
+
+    def perform_action(self, action, player):
+        # Action = 1 <= int <= Remove_stones
+        if self.is_legal_action(action):
+            self.remaining_stones -= action
+            self.print_move(action, player)
+
+    def is_legal_action(self, action):
+        return 1 <= action <= self.remove_stones
+
+    def print_move(self, action, current_player):
+        s = "Player {} selects {} stones: Remaining stones = {}".format(current_player, action, self.remaining_stones)
+        print(s)
+
 
 class OldGold(Game):
-    def __init__(self, cfg):
+    def __init__(self):
+        super().__init__()
         self.state = cfg['oldgold']['b_init']
 
-with open("config.yml", "r") as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    
-og = OldGold(cfg)
+
+Nim().init_game()
+Nim().perform_action(9, 2)
